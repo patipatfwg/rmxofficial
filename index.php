bdd274f675363a61178eda25e7a21fe288c5a86d
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-09-06 14:55:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,31 @@
 <head>
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
     <script language="javascript">
+        function loginLiff() {
+            if (!liff.isLoggedIn()) {
+                alert('loginLiff');
+                liff.login();
+            } else {
+                alert('Welcome');
+            }
+
+            window.location.reload();
+        }
+
         function initializeLiff() {
             var myLiffId = "1656005691-7qXmEbE9";
             liff.init({
                     liffId: myLiffId
                 })
                 .then(() => {
-                    initializeApp();
+                    loginLiff();
                 })
                 .catch((err) => {
                     document.getElementById("liffAppContent").classList.add('hidden');
                     document.getElementById("liffInitErrorMessage").classList.remove('hidden');
                 });
-                console.log('func initializeLiff');
-                alert('func initializeLiff');
+            console.log('func initializeLiff');
+            alert('func initializeLiff');
         }
         initializeLiff();
     </script>
