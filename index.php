d3403d33f403efd11b0e5c0fcd0ac5f37d080bf4
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-09-06 14:50:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
     <script language="javascript">
-        function initializeLiff(myLiffId) {
+        function initializeLiff() {
             var myLiffId = "1656005691-7qXmEbE9";
             liff.init({
                     liffId: myLiffId
@@ -15,12 +15,14 @@
                     document.getElementById("liffAppContent").classList.add('hidden');
                     document.getElementById("liffInitErrorMessage").classList.remove('hidden');
                 });
+                console.log('func initializeLiff');
+                alert('func initializeLiff');
         }
+        initializeLiff();
     </script>
 </head>
 
 <body>
-    <div>Hi</div>
     <div id="liffAppContent"></div>
     <div id="liffInitErrorMessage"></div>
 </body>
