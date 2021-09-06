f947392701d439542b88b463fc9c253425318a53
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-09-06 14:45:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
-<?php
+<html>
 
-echo "Hi";
+<head>
+    <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
+    <script language="javascript">
+        function initializeLiff(myLiffId) {
+            var myLiffId = "1656005691-7qXmEbE9";
+            liff.init({
+                    liffId: myLiffId
+                })
+                .then(() => {
+                    initializeApp();
+                })
+                .catch((err) => {
+                    document.getElementById("liffAppContent").classList.add('hidden');
+                    document.getElementById("liffInitErrorMessage").classList.remove('hidden');
+                });
+        }
+    </script>
+</head>
 
-?>
-HHHHH
-<script>
-alert('Hi');
-</script>
+<body>
+    <div id="liffAppContent"></div>
+    <div id="liffInitErrorMessage"></div>
+</body>
+
+</html>
