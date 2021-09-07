62906636dcddd17908c8c9e191895e62a46909c7
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-09-07 11:23:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,42 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://static.line-scdn.net/liff/edge/versions/2.12.0/sdk.js"></script>
-
+    <title>LIFF - LINE RMX-E Official Dev</title>
 </head>
 
 <body>
-    Test
-
+    <hr>Test
+    <hr>
+    <div id="errorMsg"></div>
     <script language="javascript">
-        async function checkUserId() {
-            if (!liff.isLoggedIn()) {
-                console.log('func loginLiff');
-                await liff.login();
-            } else {
-                alert('Welcome');
+        $(document).ready(function() {
+            async function checkUserId() {
+                window.location.reload();
             }
 
-            window.location.reload();
-        }
-
-        async function initializeLiff() {
-            console.log('func initializeLiff');
-            myLiffId = "1656005691-7qXmEbE9";
-            await liff.init({
-                    liffId: myLiffId
-                })
-                .then(() => {
-                    if (liff.isLoggedIn()) {
-                        // checkUserId();
-                        alert('checkUserId');
-                    } else {
-                        liff.login();
-                    }
-                })
-                .catch((err) => {
-                    alert(err);
-                });
-        }
-        initializeLiff();
+            async function initializeLiff() {
+                console.log('func initializeLiff');
+                myLiffId = "1656005691-7qXmEbE9";
+                await liff.init({
+                        liffId: myLiffId
+                    })
+                    .then(() => {
+                        if (liff.isLoggedIn()) {
+                            checkUserId();
+                        } else {
+                            liff.login();
+                        }
+                    })
+                    .catch((err) => {
+                       $("#errorMsg").text(err)
+                    });
+            }
+            initializeLiff();
+        });
     </script>
 </body>
 
