08ea1951889266487331cc33ae84f5ef1fd0e15d
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-09-13 22:31:05 +0700

    Update callApi.php

--- callApi.php
+++ callApi.php
@@ -2,10 +2,10 @@
 
 //กำหนดค่า Access-Control-Allow-Origin ให้ เครื่อง อื่น ๆ สามารถเรียกใช้งานหน้านี้ได้
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+// header("Content-Type: application/json; charset=UTF-8");
+// header("Access-Control-Max-Age: 3600");
+// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 function dbConnect()
 {
