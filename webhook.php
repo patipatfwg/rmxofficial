0f9e5c6d2365f7613c86f8d7e5371a06d8ccdca0
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-09-27 22:19:28 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,26 +1,41 @@
 <?php
-function sendMessage($replyJson, $sendInfo)
+function sendMessage($replyJson)
 {
-    $ch = curl_init($sendInfo["URL"]);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt(
-        $ch,
-        CURLOPT_HTTPHEADER,
-        array(
-            'Content-Type: application/json',
-            'Authorization: Bearer ' . $sendInfo["AccessToken"]
-        )
-    );
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
+    $sendInfo['URL'] = "https://api.line.me/v2/bot/message/reply";
+    $sendInfo['AccessToken'] = "s2l19GfGgdDnsbO9cidJGvlkKDvlT9MRiQla/SKo63c3Us7Tv/xKjLnkLnafX15C3U9N9AT5FiL/ARZHWhicfAqm7bSmB1TJWFAzYkBxgSdZbHVKMag6WdTUtnsb56UmvcwbxVq5WUiRzRfTcLTv9QdB04t89/1O/w1cDnyilFU=";
+
+    try {
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $sendInfo['URL']);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt(
+            $ch,
+            CURLOPT_POSTFIELDS,
+            $replyJson
+        );
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            array(
+                'Content-Type: application/json',
+                'Authorization: Bearer ' . $sendInfo["AccessToken"]
+            )
+        );
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); # receive server response
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); # do not verify SSL
+        $data = curl_exec($ch); # execute curl
+        $httpstatus = curl_getinfo($ch, CURLINFO_HTTP_CODE); # http response status code
+        curl_close($ch);
+
+        $data = $data;
+    } catch (Exception $ex) {
+        $data = $ex;
+    }
+    return $data;
 }
 
-$lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
-$lineData['AccessToken'] = "s2l19GfGgdDnsbO9cidJGvlkKDvlT9MRiQla/SKo63c3Us7Tv/xKjLnkLnafX15C3U9N9AT5FiL/ARZHWhicfAqm7bSmB1TJWFAzYkBxgSdZbHVKMag6WdTUtnsb56UmvcwbxVq5WUiRzRfTcLTv9QdB04t89/1O/w1cDnyilFU=";
 
 $LINEData = file_get_contents('php://input');
 $jsonData = json_decode($LINEData, true);
@@ -34,6 +49,6 @@ $replyJson["messages"][0] = $replyText;
 
 $encodeJson = json_encode($replyJson);
 
-$results = sendMessage($encodeJson, $lineData);
+$results = sendMessage($encodeJson);
 echo $results;
 http_response_code(200);
