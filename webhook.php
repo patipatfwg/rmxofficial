5fb05faa08b4d13286905fbbef1c28b8ea482a5e
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-09-27 18:29:14 +0700

    1829

--- webhook.php
+++ webhook.php
@@ -1,43 +1,50 @@
 <?php
-    function sendMessage($replyJson, $sendInfo)
-    {
-        $ch = curl_init($sendInfo["URL"]);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt(
-            $ch,
-            CURLOPT_HTTPHEADER,
-            array(
-                'Content-Type: application/json',
-                'Authorization: Bearer ' . $sendInfo["AccessToken"]
-            )
-        );
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        return $result;
-    }
+function sendMessage($replyJson, $sendInfo)
+{
+    $ch = curl_init($sendInfo["URL"]);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt(
+        $ch,
+        CURLOPT_HTTPHEADER,
+        array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $sendInfo["AccessToken"]
+        )
+    );
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
 
 
-    $LINEData = file_get_contents('php://input');
-    $jsonData = json_decode($LINEData, true);
+$LINEData = file_get_contents('php://input');
+$jsonData = json_decode($LINEData, true);
 
-    $Name = "HIHI";
-    $Surname = "GOGO";
-    $replyText["text"] = "สวัสดีคุณ $Name $Surname";
+//เก็บ ReplyToken
+$replyToken = $jsonData["events"][0]["replyToken"];
+// เก็บ userID
+$userID = $jsonData["events"][0]["source"]["userId"];
+// เก็บ text
+$text = $jsonData["events"][0]["message"]["text"];
+// เก็บ Timestamp
+$timestamp = $jsonData["events"][0]["timestamp"];
 
-    $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
-    $lineData['AccessToken'] = "s2l19GfGgdDnsbO9cidJGvlkKDvlT9MRiQla/SKo63c3Us7Tv/xKjLnkLnafX15C3U9N9AT5FiL/ARZHWhicfAqm7bSmB1TJWFAzYkBxgSdZbHVKMag6WdTUtnsb56UmvcwbxVq5WUiRzRfTcLTv9QdB04t89/1O/w1cDnyilFU=";
+$encodeJson = json_encode($replyJson);
 
-    $replyJson["replyToken"] = $replyToken;
-    $replyJson["messages"][0] = $replyText;
+$Name = "HIHI";
+$Surname = "GOGO";
+$replyText["text"] = "สวัสดีคุณ $Name $Surname";
 
-    $encodeJson = json_encode($replyJson);
+$lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
+$lineData['AccessToken'] = "s2l19GfGgdDnsbO9cidJGvlkKDvlT9MRiQla/SKo63c3Us7Tv/xKjLnkLnafX15C3U9N9AT5FiL/ARZHWhicfAqm7bSmB1TJWFAzYkBxgSdZbHVKMag6WdTUtnsb56UmvcwbxVq5WUiRzRfTcLTv9QdB04t89/1O/w1cDnyilFU=";
 
 
-    $results = sendMessage($encodeJson, $lineData);
-    echo $results;
-    http_response_code(200);
 
-?>
+
+
+$results = sendMessage($encodeJson, $lineData);
+echo $results;
+http_response_code(200);
